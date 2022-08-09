8c2f730f3d53242b16b73db3a807b58e77bd0692
fatonianggris <mfatonianggris@gmail.com>
2022-08-09 17:17:20 +0700

    update date

--- employee/employe/views/employe_list_presence_all_month.php
+++ employee/employe/views/employe_list_presence_all_month.php
@@ -67,18 +67,18 @@
 															<label>Bulan:</label>
 															<select class="form-control datatable-input" data-col-index="10">
 																<option value="">Pilih Bulan</option>
-																<option value="0">Januari</option>
-																<option value="1">Februari</option>
-																<option value="2">Maret</option>
-																<option value="3">April</option>
-																<option value="4">Mei</option>
-																<option value="5">Juni</option>
-																<option value="6">Juli</option>
-																<option value="7">Agustus</option>
-																<option value="8">September</option>
-																<option value="9">Oktober</option>
-																<option value="10">November</option>
-																<option value="11">Desember</option>
+																<option value="1">Januari</option>
+																<option value="2">Februari</option>
+																<option value="3">Maret</option>
+																<option value="4">April</option>
+																<option value="5">Mei</option>
+																<option value="6">Juni</option>
+																<option value="7">Juli</option>
+																<option value="8">Agustus</option>
+																<option value="9">September</option>
+																<option value="10">Oktober</option>
+																<option value="11">November</option>
+																<option value="12">Desember</option>
 																<option value="">Semua</option>
 															</select>
 														</div>
